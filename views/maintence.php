5d894ed55c531e2c2ecddf5f93501824c44e68b4
ChristinaSW <cwoody@anioncreative.com>
2021-11-18 06:46:05 -0500

    Add front end html & css code

---
+++ views/maintence.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php echo plugins_url( 'assets/style.css', dirname( __FILE__ ) ); ?>">
+        <title>Under Maintenance</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="main-logo">
+                <img src="https://d3or43zegi42q6.cloudfront.net/2020/12/logo.png">
+            </div>
+        </div>
+    </body>
+</html>
