25eaa996466b67399c86891dacdbd508828c433f
ChristinaSW <53832305+ChristinaSW@users.noreply.github.com>
2022-08-05 14:23:49 -0400

    Update aic-theme-options.php

--- aic-theme-options.php
+++ aic-theme-options.php
@@ -4,7 +4,7 @@
  * Plugin Name: AIC Theme Options
  * Plugin URI: https://anioncreative.com
  * Description: Adds user options to AIC theme.
- * Version: 7.3
+ * Version: 7.4
  * Author: An Ion Creative
  * Author URI: https://anioncreative.com
  *
