9f6daffa60b15b9dc592b32940858c6ee86304c3
ChristinaSW <53832305+ChristinaSW@users.noreply.github.com>
2023-02-14 11:11:54 -0500

    Update updater.php

--- lib/updater.php
+++ lib/updater.php
@@ -25,7 +25,6 @@ class Smashing_Updater {
 		add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );
 
 		return $this;
-		print_r($this);
 	}
 
 	public function set_plugin_properties() {
@@ -47,65 +46,51 @@ class Smashing_Updater {
 	}
 
 	private function get_repository_info() {
-	    if ( is_null( $this->github_response ) ) { // Do we have a response?
-		$args = array();
-	        $request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
-		    
-		$args = array();
+		if ( is_null( $this->github_response ) ) { // Do we have a response?
+			$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
+			if( $this->authorize_token ) { // Is there an access token?
+				$request_uri = sprintf( '%s?access_token=%s', $request_uri, $this->authorize_token ); // Append it
+			}
 
-	        if( $this->authorize_token ) { // Is there an access token?
-		          $args['headers']['Authorization'] = "bearer {$this->authorize_token}"; // Set the headers
-	        }
+			$response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
+			if( is_array( $response) ) { // If it is an array
+				$response = current( $response ); // Get the first item
+			}
 
-	        $response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri, $args ) ), true ); // Get JSON and parse it
+			if( $this->authorize_token ) { // Is there an access token?
+				 //$response['zipball_url'] = sprintf( '%s?access_token=%s', $response['zipball_url'], $this->authorize_token ); // Update our zip url with token
+				 $response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] ); // Update our zip url with token
+			}
 
-	        if( is_array( $response ) ) { // If it is an array
-	            $response = current( $response ); // Get the first item
-	        }
-
-	        $this->github_response = $response; // Set it to our property
-	    }
+			$this->github_response = $response; // Set it to our property			 
+		}
 	}
 
 	public function initialize() {
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'modify_transient' ), 10, 1 );
 		add_filter( 'plugins_api', array( $this, 'plugin_popup' ), 10, 3);
 		add_filter( 'upgrader_post_install', array( $this, 'after_install' ), 10, 3 );
-		
-		// Add Authorization Token to download_package
-		add_filter( 'upgrader_pre_download',
-			function() {
-				add_filter( 'http_request_args', [ $this, 'download_package' ], 15, 2 );
-				return false; // upgrader_pre_download filter default return value.
-			}
-		);
 	}
 
 	public function modify_transient( $transient ) {
+		if( $checked = $transient->checked ) { // Did Wordpress check for updates?
 
-		if( property_exists( $transient, 'checked') ) { // Check if transient has a checked property
+			$this->get_repository_info(); // Get the repo info
 
-			if( $checked = $transient->checked ) { // Did Wordpress check for updates?
+			$out_of_date = version_compare( $this->github_response['tag_name'], $checked[ $this->basename ] ); // Check if we're out of date
 
-				$this->get_repository_info(); // Get the repo info
+			if( $out_of_date ) {
 
-				$out_of_date = version_compare( $this->github_response['tag_name'], $checked[ $this->basename ], 'gt' ); // Check if we're out of date
+				$new_files = $this->github_response['zipball_url']; // Get the ZIP
 
-				if( $out_of_date ) {
+				$plugin = array( // setup our plugin info
+					'url' => $this->plugin["PluginURI"],
+					'slug' => $this->basename,
+					'package' => $new_files,
+					'new_version' => $this->github_response['tag_name']
+				);
 
-					$new_files = $this->github_response['zipball_url']; // Get the ZIP
-
-					$slug = current( explode('/', $this->basename ) ); // Create valid slug
-
-					$plugin = array( // setup our plugin info
-						'url' => $this->plugin["PluginURI"],
-						'slug' => $slug,
-						'package' => $new_files,
-						'new_version' => $this->github_response['tag_name']
-					);
-
-					$transient->response[$this->basename] = (object) $plugin; // Return it in response
-				}
+				$transient->response[$this->basename] = (object) $plugin; // Return it in response
 			}
 		}
 
@@ -113,10 +98,10 @@ class Smashing_Updater {
 	}
 
 	public function plugin_popup( $result, $action, $args ) {
-	
+
 		if( ! empty( $args->slug ) ) { // If there is a slug
-			
-			if( $args->slug == current( explode( '/' , $this->basename ) ) ) { // And it's our slug
+
+			if( $args->slug == $this->basename ) { // And it's our slug
 
 				$this->get_repository_info(); // Get our repo info
 
@@ -124,49 +109,30 @@ class Smashing_Updater {
 				$plugin = array(
 					'name'				=> $this->plugin["Name"],
 					'slug'				=> $this->basename,
-					'requires'			=> '3.3',
-					'tested'			=> '6.0.1',
-					'rating'			=> '100.0',
-					'num_ratings'		=> '10823',
-					'downloaded'		=> '14249',
-					'added'				=> '2016-01-05',
 					'version'			=> $this->github_response['tag_name'],
 					'author'			=> $this->plugin["AuthorName"],
 					'author_profile'	=> $this->plugin["AuthorURI"],
 					'last_updated'		=> $this->github_response['published_at'],
 					'homepage'			=> $this->plugin["PluginURI"],
 					'short_description' => $this->plugin["Description"],
-					'sections'			=> array(
+					'sections'			=> array( 
 						'Description'	=> $this->plugin["Description"],
-						'Updates'		=> $this->github_response['body'],
+						'Updates'		=> $this->github_response['body']
 					),
-					'download_link'		=> $this->github_response['zipball_url']
+					'download_link'		=> $this->github_response['zipball_url'],
 				);
 
 				return (object) $plugin; // Return the data
 			}
 
-		}
+		}	
 		return $result; // Otherwise return default
-	}
-	
-	public function download_package( $args, $url ) {
-
-		if ( null !== $args['filename'] ) {
-			if( $this->authorize_token ) { 
-				$args = array_merge( $args, array( "headers" => array( "Authorization" => "token {$this->authorize_token}" ) ) );
-			}
-		}
-		
-		remove_filter( 'http_request_args', [ $this, 'download_package' ] );
-
-		return $args;
 	}
 
 	public function after_install( $response, $hook_extra, $result ) {
 		global $wp_filesystem; // Get global FS object
 
-		$install_directory = plugin_dir_path( $this->file ); // Our plugin directory
+		$install_directory = plugin_dir_path( $this->file ); // Our plugin directory 
 		$wp_filesystem->move( $result['destination'], $install_directory ); // Move files to the plugin dir
 		$result['destination'] = $install_directory; // Set the destination for the rest of the stack
 
