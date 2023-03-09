fd725c85f6e97ee7b3df62d28f0970da56852b22
ChristinaSW <53832305+ChristinaSW@users.noreply.github.com>
2023-03-09 10:16:12 -0500

    Update aic-theme-options.php

--- aic-theme-options.php
+++ aic-theme-options.php
@@ -396,19 +396,28 @@ Background Color: has-'.$color_name.'-background-color';
         return $excluded_ids;
     }
 
-    function aic_disable_gutenberg( $can_edit, $post_type ) {
+//     function aic_disable_gutenberg( $can_edit, $post_type ) {
 
-        if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
-            return $can_edit;
+//         if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+//             return $can_edit;
 
-        if( aic_disable_editor( $_GET['post'] ) )
-            $can_edit = false;
+//         if( aic_disable_editor( $_GET['post'] ) )
+//             $can_edit = false;
 
-        return $can_edit;
+//         return $can_edit;
 
     }
-    add_filter( 'gutenberg_can_edit_post_type', 'aic_disable_gutenberg', 10, 2 );
-    add_filter( 'use_block_editor_for_post_type', 'aic_disable_gutenberg', 10, 2 );
+//     add_filter( 'gutenberg_can_edit_post_type', 'aic_disable_gutenberg', 10, 2 );
+//     add_filter( 'use_block_editor_for_post_type', 'aic_disable_gutenberg', 10, 2 );
 
+    function aic_disable_gutenberg( $use_block_editor, $post ) {
+
+        $excluded_ids = get_field('disable_editor_list', 'option');
+        if ( in_array( $post->ID, $excluded_ids ) ) {
+            return false;
+        }
+        return $use_block_editor;
+    }
+    add_filter( 'use_block_editor_for_post', 'aic_disable_gutenberg', 10, 2 );
 
 ?>
